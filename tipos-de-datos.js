1f58dfa665d2f4facbd84aa517f2ac213ff028ac
BenjaminArroyo <benji_rikoflishas@hotmail.com>
2024-11-23 21:41:33 -0700

    first commit. Agregando archivos, incluyendo entregable, archivo de la clase y la calculadora

---
+++ tipos-de-datos.js
@@ -0,0 +1,9 @@
+console.log(typeof 42);
+console.log(typeof 'Veinticinco');
+console.log(typeof -666);
+console.log(typeof true);
+console.log(typeof 0);
+console.log(typeof "");
+console.log(typeof null);
+console.log(typeof undefined);
+console.log(typeof FALSE);
